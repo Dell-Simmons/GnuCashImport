d290263033daad3459d48e92f49c0b05bb19936b
Dell <Dell.Simmons@gmail.com>
2026-05-23 09:35:56 -0700

    Refactor LocalDbConnectionManager for clarity and structure
    Removed unused code, organized token methods into regions, made FeeBayOAuthTokensRepository readonly and initialized via a static method, and streamlined repository initialization and SaveUserToken logic.

--- LocalDBConnections/LocalDBConnectionManager.cs
+++ LocalDBConnections/LocalDBConnectionManager.cs
@@ -4,7 +4,6 @@ using MicroOrm.Dapper.Repositories.SqlGenerator;
 using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Threading.Tasks;
 
 namespace LocalDBConnections
 {
@@ -12,56 +11,44 @@ namespace LocalDBConnections
     {
         #region Constants and Fields
         private const string StampDataConnection = "Data Source=SERVER\\SQLEXPRESS;Initial Catalog=StampData;User ID=GenericUser;Password=Mishmash@2!;TrustServerCertificate=True";
-        
-      //  private readonly IConfiguration _configuration;
-        //private readonly ILogger _logger;
-       // private readonly string _pathToStockImages;
-        private FeeBayOAuthTokensRepository _feeBayOAuthTokensRepository;
-     //   private LogRepository _logRepository;
+
+        private readonly FeeBayOAuthTokensRepository _feeBayOAuthTokensRepository;
         #endregion
 
-        #region Events
-      //  public event System.EventHandler<MyEventArgs<string>> DataBaseTableChanged;
-        #endregion
-        
-     //   public string PathToStockImages { get { return _pathToStockImages; } }
-        
         #region Constructors
         public LocalDbConnectionManager()
         {
-         //   _logger = logger;
-          //  _configuration = configuration;
-            SetUpStampDataRepositories(StampDataConnection);
-        //    _ = DeleteOldNLogRecordsAsync(24);
-        //    _pathToStockImages = _configuration.GetValue<string>("Scans:PathToStockImages");
+            _feeBayOAuthTokensRepository = CreateFeeBayOAuthTokensRepository(StampDataConnection);
         }
         #endregion
 
-        #region Methods
+        #region Token Read Operations
         public string GetRefreshToken(string feeBayUser)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == feeBayUser);
             return feeBayOAuthTokenData.RefreshToken;
         }
-        
+
         public DateTime GetRefreshTokenExpireTime(string feeBayUser)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == feeBayUser);
             return feeBayOAuthTokenData.RefreshTokenExpire;
         }
-        
+
         public string GetUserToken(string feeBayUser)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == feeBayUser);
             return feeBayOAuthTokenData.OAuthToken;
         }
-        
+
         public DateTime GetUserTokenExpireTime(string feeBayUser)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == feeBayUser);
             return feeBayOAuthTokenData.OAuthTokenExpire;
         }
-        
+        #endregion
+
+        #region Token Write Operations
         public bool SaveUserToken(FeeBayOAuthTokens tokens)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == tokens.FeeBayUserName);
@@ -70,25 +57,25 @@ namespace LocalDBConnections
             feeBayOAuthTokenData.OAuthTokenExpire = tokens.OAuthTokenExpire;
             feeBayOAuthTokenData.RefreshToken = tokens.RefreshToken;
             feeBayOAuthTokenData.RefreshTokenExpire = tokens.RefreshTokenExpire;
-            var success = _feeBayOAuthTokensRepository.Update(feeBayOAuthTokenData);
-            return success;
+
+            return _feeBayOAuthTokensRepository.Update(feeBayOAuthTokenData);
         }
-        
+
         public bool SaveUserToken(string access_token, DateTime expires_in, string feeBayUser)
         {
             var feeBayOAuthTokenData = _feeBayOAuthTokensRepository.Find(l => l.FeeBayUserName == feeBayUser);
             feeBayOAuthTokenData.OAuthToken = access_token;
             feeBayOAuthTokenData.OAuthTokenExpire = expires_in;
-            var success = _feeBayOAuthTokensRepository.Update(feeBayOAuthTokenData);
-            return success;
+
+            return _feeBayOAuthTokensRepository.Update(feeBayOAuthTokenData);
         }
-        
-     
-        private void SetUpStampDataRepositories(string stampDataConnection)
+        #endregion
+
+        #region Repository Initialization
+        private static FeeBayOAuthTokensRepository CreateFeeBayOAuthTokensRepository(string stampDataConnection)
         {
             IDbConnection dbConnection = new SqlConnection(stampDataConnection);
-            _feeBayOAuthTokensRepository = new FeeBayOAuthTokensRepository(dbConnection, new SqlGenerator<FeeBayOAuthTokens>());
-         //   _logRepository = new LogRepository(stampDataConnection);
+            return new FeeBayOAuthTokensRepository(dbConnection, new SqlGenerator<FeeBayOAuthTokens>());
         }
         #endregion
     }
