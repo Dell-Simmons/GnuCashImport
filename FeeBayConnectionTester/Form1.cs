8bb70e0dce2961ac7e1d40837094569f4ec783d7
Dell <Dell.Simmons@gmail.com>
2026-05-30 01:32:07 -0700

    #4 Update transaction summary logic and add date utility
    Refactored button1_Click to fetch transaction summaries for PAYOUT and COMPLETED statuses using combined filters. Added ToEbayDate utility method for formatting dates. Improved comments and code organization. Added necessary using statements for eBay entities.

--- FeeBayConnectionTester/Form1.cs
+++ FeeBayConnectionTester/Form1.cs
@@ -1,5 +1,7 @@
 using EbaySharp.Controllers;
 using EbaySharp.Entities.Develop.KeyManagement.SigningKey;
+using EbaySharp.Entities.Develop.SellingApps.AccountManagement.Finances.Transaction;
+using EbaySharp.Entities.Develop.SellingApps.OrderManagement.Fulfillment.Order;
 using FeeBayConnectionTester.Extensions;
 using FeeBayOAuth.TokenService;
 using LocalDBConnections;
@@ -29,11 +31,6 @@ namespace FeeBayConnectionTester
         #endregion
 
         #region Event handlers
-        #region
-        //public Form1()
-        //{
-        //    InitializeComponent();
-        //}
         private async void button1_Click(object sender, EventArgs e)
         {
             // token identifies the user and application,
@@ -44,45 +41,56 @@ namespace FeeBayConnectionTester
             // to verify the identity of the requester and ensure they have the necessary
             // permissions to perform the requested actions.
             string? token = await _oAuthTokenService.GetOAuthTokenAsync("Simmons_Ink");
+           
             var ebayController = _ebayControllerFactory(token);
             // The signing key is used to create digital signatures for API requests,
             // it is associated with the application
             // and is used to ensure the integrity and authenticity of the requests.
+            // The one stored in the database is good for 3 years from today (5/29/26).
+            // So don't fucking worry about it expiring anytime soon.
             var signingKey = await GetOrCreateSigningKey(ebayController);
 
-            var asdf = await ebayController.GetTransactionSummary(signingKey, "transactionType:{SALE}");
-        }
-        #endregion
+            string multiFilter;
 
-        #region
+            // Combine multiple filters into ONE comma-separated string
+
+            //!{PAYOUT} is funds going from feeBay to bank account
+            multiFilter = "transactionStatus:{PAYOUT},transactionDate:[2026-01-01T00:00:00.000Z..2026-01-31T23:59:59.000Z]";
+
+            TransactionSummary transactionPayoutSummary = 
+                await ebayController.GetTransactionSummary(signingKey, multiFilter);
+            //!{ COMPLETED} is funds going from buyer to feeBay.
+            multiFilter = "transactionStatus:{COMPLETED},transactionDate:[2026-01-01T00:00:00.000Z..2026-01-31T23:59:59.000Z]";
+
+            TransactionSummary transactionCompletedSummary =
+                await ebayController.GetTransactionSummary(signingKey, multiFilter);
+
+
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-        #endregion
         #endregion
         #region Methods
         #region Private Methods
         private async Task<SigningKey> GetOrCreateSigningKey(EbayController ebayController)
         {
-           // // 1. Try to get from database
+            // // 1. Try to get from database
             FeeBaySigningKeys? cachedKey = null;
 
             cachedKey = await _localDbConnectionManager.GetSigningKeyAsync();
 
-           //// cachedKey = null; // Force create new key for testing
-            if (cachedKey != null)
+            //// cachedKey = null; // Force create new key for testing
+            if(cachedKey != null)
             {
                 return cachedKey.ToSigningKey();
             }
 
-           // // 2. Request from eBay
-           // var signingKeys = await ebayController.GetSigningKeys();
+
             SigningKey key;
 
-           // if(signingKeys?.SigningKeyList?.Length > 0)
-           // {
-           //     key = signingKeys.SigningKeyList[0];
-           // } else
+
             {
                 // 3. Create new if none exist
                 key = await ebayController.CreateSigningKey();
@@ -92,6 +100,10 @@ namespace FeeBayConnectionTester
             await _localDbConnectionManager.SaveSigningKeyAsync(key.ToFeeBaySigningKey());
 
             return key;
+        }
+        public static string ToEbayDate(DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime().ToString("o");
         }
         #endregion
         #endregion
