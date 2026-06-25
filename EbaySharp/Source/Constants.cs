120ecf8c406490124c219dc0ebdbc812e2827f59
Dell <Dell.Simmons@gmail.com>
2026-06-25 11:16:01 -0700

    Bug in EbaySharp\Source\Constants caused limit and offset to not work for GetPayouts
    Add pagination to GET_PAYOUTS endpoint constant
    
    Updated the GET_PAYOUTS constant to include limit and offset query parameters, enabling pagination support for payout retrieval.

--- EbaySharp/Source/Constants.cs
+++ EbaySharp/Source/Constants.cs
@@ -33,7 +33,7 @@
                             internal const string GET_TRANSACTIONS = "/transaction?limit={0}&offset={1}";
                             internal const string GET_TRANSACTION_SUMMARY = "/transaction_summary";
                             internal const string GET_PAYOUT_SUMMARY = "/payout_summary";
-                            internal const string GET_PAYOUTS = "/payout";
+                            internal const string GET_PAYOUTS = "/payout?limit={0}&offset={1}";
                             internal const string GET_PAYOUT = "/payout/{0}";
                         }
                     }
