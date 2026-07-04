57cef1dde25338fe76a7b2e29dd7634177f9f8eb
Dell <Dell.Simmons@gmail.com>
2026-07-04 09:06:05 -0700

    #9 Add new fields to SimpleFinTransaction for more details
    Expanded SimpleFinTransaction with TransactionId, Payee, Memo, TransactedAtDateUnix, and Mcc properties, each mapped to their respective JSON fields. Updated using directives to support these changes.

--- SimpleFin/SimpleFinDTO/SimpleFinTransaction.cs
+++ SimpleFin/SimpleFinDTO/SimpleFinTransaction.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
 using System.Text.Json.Serialization;
 namespace SimpleFin.SimpleFinDTO
 {
    
     public class SimpleFinTransaction
     {
+        [JsonPropertyName("id")]
+        public string TransactionId { get; set; }
         [JsonPropertyName("posted")]
         public long PostedDateUnix { get; set; }
         public DateTimeOffset PostedDate { get { return DateTimeOffset.FromUnixTimeSeconds(PostedDateUnix); } }
@@ -14,5 +17,13 @@ namespace SimpleFin.SimpleFinDTO
 
         [JsonPropertyName("description")]
         public string Description { get; set; } = "";
+        [JsonPropertyName("payee")]
+        public string Payee { get; set; }
+        [JsonPropertyName("memo")]
+        public string Memo { get; set; }
+        [JsonPropertyName("transacted_at")]
+        public long TransactedAtDateUnix { get; set; }
+        [JsonPropertyName("mcc")]
+        public string Mcc { get; set; }
     }
 }
